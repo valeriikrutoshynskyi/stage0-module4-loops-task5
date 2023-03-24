c51304ab6297264234aec3b341277072b3cf62cd
Valerii Krutoshynskyi <valera9496@gmail.com>
2023-03-24 15:05:11 +0100

    Hourglass

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        int row, col, space = 0;
+        for (row = 1; row <= height; row++) {
+            for (col = 1; col <= height; col++) {
+                if (col <= space || col > height - space) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("8");
+                }
+            }
+            if (row <= height / 2) {
+                space++;
+            } else {
+                space--;
+            }
+            System.out.println();
+        }
     }
 }
